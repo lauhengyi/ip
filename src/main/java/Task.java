7c56fc5e8b106dcd2ee8ce20fea3961af2dbd6ed
Lau Heng Yi <77540391+lauhengyi@users.noreply.github.com>
2025-01-22 14:59:26 +0800

    Added task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+    private final String description;
+    private boolean isDone;
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void mark() {
+        this.isDone = true;
+    }
+
+    public void unmark() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        String symbol = isDone ? "[X]" : "[ ]";
+        return symbol + this.description;
+    }
+}
