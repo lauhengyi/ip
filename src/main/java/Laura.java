54106c5d083af5002592395fd78ab349570e1714
Lau Heng Yi <77540391+lauhengyi@users.noreply.github.com>
2025-01-22 14:50:44 +0800

    Added list feature

--- src/main/java/Laura.java
+++ src/main/java/Laura.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Laura{
@@ -9,6 +10,7 @@ public class Laura{
                        "What can I do for you?"
         );
 
+        ArrayList<String> list = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
@@ -16,8 +18,16 @@ public class Laura{
                 Message.send("Bye. Hope to see you again soon!");
                 break;
             }
-
-            Message.send(input);
+            if (input.equals("list")) {
+                StringBuilder message = new StringBuilder();
+                for (int i = 0; i < list.size(); i++) {
+                    message.append(i + 1).append(". ").append(list.get(i)).append("\n");
+                }
+                Message.send(message.toString());
+            } else {
+                list.add(input);
+                Message.send("Added: " + input);
+            }
         }
 
 
