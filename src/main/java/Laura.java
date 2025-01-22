f1f44caafa893740db95e84aff50c151c5ea51bf
Lau Heng Yi <77540391+lauhengyi@users.noreply.github.com>
2025-01-22 14:38:04 +0800

    Added message class for messaging

--- src/main/java/Laura.java
+++ src/main/java/Laura.java
@@ -3,26 +3,21 @@ import java.util.Scanner;
 public class Laura{
     public static void main(String[] args) {
         String name = "Laura";
-        String line = "____________________________________________________________\n";
 
-        System.out.println(line);
-        System.out.println("Hello I'm " + name);
-        System.out.println("What can I do for you?");
-        System.out.println(line);
+        Message.send(
+               "Hello I'm " + name + "\n" +
+                       "What can I do for you?"
+        );
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
             if (input.equals("bye")) {
-                System.out.println(line);
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println(line);
+                Message.send("Bye. Hope to see you again soon!");
                 break;
             }
 
-            System.out.println(line);
-            System.out.println(input);
-            System.out.println(line);
+            Message.send(input);
         }
 
 
---
+++ src/main/java/Message.java
@@ -0,0 +1,8 @@
+public class Message {
+    public static void send(String message) {
+        String line = "____________________________________________________________";
+        System.out.println("\n" + line);
+        System.out.println(message);
+        System.out.println(line);
+    }
+}
