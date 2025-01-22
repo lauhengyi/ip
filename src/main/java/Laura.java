e49bee7baacd002f3d443d2b2959a333e204ed41
Lau Heng Yi <77540391+lauhengyi@users.noreply.github.com>
2025-01-22 14:26:35 +0800

    Added echoing

--- src/main/java/Laura.java
+++ src/main/java/Laura.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Laura{
     public static void main(String[] args) {
         String name = "Laura";
@@ -7,7 +9,22 @@ public class Laura{
         System.out.println("Hello I'm " + name);
         System.out.println("What can I do for you?");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(line);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+
+            System.out.println(line);
+            System.out.println(input);
+            System.out.println(line);
+        }
+
+
     }
 }
