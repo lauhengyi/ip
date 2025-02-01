1cf9b72f6599b7df4812561d7d2e9510a4599060
lauhengyi <lauhengyi@gmail.com>
2025-02-01 18:23:02 +0800

    Add docs for tasks

--- src/main/java/laura/task/EventTask.java
+++ src/main/java/laura/task/EventTask.java
@@ -3,16 +3,32 @@ package laura.task;
 import laura.Date;
 import laura.exception.LauraException;
 
+/**
+ * A Task with a start and end date
+ */
 public class EventTask extends Task {
     final Date from;
     final Date to;
 
+    /**
+     * @param description The Task's name
+     * @param from The start date of the Task
+     * @param to The end date of the Task
+     * @throws LauraException If any of the dates is in an incorrect format
+     */
     public EventTask(String description, String from, String to) throws LauraException {
         super(description);
         this.from = new Date(from);
         this.to = new Date(to);
     }
 
+    /**
+     * @param isDone Whether the task is marked or not
+     * @param description The Task's name
+     * @param from The start date of the Task
+     * @param to The end date of the Task
+     * @throws LauraException If any of the dates is in an incorrect format
+     */
     public EventTask(boolean isDone, String description, String from, String to) throws LauraException {
         super(isDone, description);
         this.from = new Date(from);
--- src/main/java/laura/task/Task.java
+++ src/main/java/laura/task/Task.java
@@ -1,27 +1,52 @@
 package laura.task;
 
+/**
+ * The parent for all Tasks
+ */
 public abstract class Task {
     private final String description;
+    /**
+     * Whether the Task is marked or not
+     */
     private boolean isDone;
 
+    /**
+     * Create a Task instance
+     * @param description The Task's Name
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Create a Task instance
+     * @param isDone Whether the task is marked
+     * @param description What the Task is named
+     */
     public Task(boolean isDone, String description) {
         this.description = description;
         this.isDone = isDone;
     }
 
+    /**
+     * Mark the Task
+     */
     public void mark() {
         this.isDone = true;
     }
 
+    /**
+     * Unmark the Task
+     */
     public void unmark() {
         this.isDone = false;
     }
 
+    /**
+     * Get the encoded version of the Task for storage on local
+     * @return The encoded string of the Task
+     */
     public String encode() {
         String encodedDone = isDone ? "1" : "0";
         return encodedDone + "|" + this.description;
